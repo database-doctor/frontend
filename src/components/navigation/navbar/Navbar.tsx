ad7d1c4ce6a854638726068cab3cabc814cbbf40
Adrian Davila <akdavila03@gmail.com>
2023-07-08 15:01:09 -0400

    add toggle login component for testing the store temporarily

--- src/components/navigation/navbar/Navbar.tsx
+++ src/components/navigation/navbar/Navbar.tsx
@@ -1,14 +1,32 @@
 "use client";
 
 import React from "react";
-import { useAppSelector } from "@/hooks";
-import { selectAuthState, selectUser } from "@/store/features/authSlice";
+import { useAppDispatch, useAppSelector } from "@/hooks";
+import {
+  selectAuthState,
+  selectUser,
+  setAuthState,
+} from "@/store/features/authSlice";
 import { User } from "@/types/User";
+import { Button } from "@chakra-ui/react";
 
 function Navbar() {
   const authState: boolean = useAppSelector(selectAuthState);
   const user: User | undefined = useAppSelector(selectUser);
-  return <div>Logged in: {authState ? "true" : "false"}</div>;
+  const dispatch = useAppDispatch();
+
+  const toggleAuthState = () => {
+    dispatch(setAuthState(!authState));
+  };
+
+  return (
+    <>
+      <div>Logged in: {authState ? "true" : "false"}</div>
+      <Button colorScheme="pink" onClick={toggleAuthState}>
+        Toggle Auth State
+      </Button>
+    </>
+  );
 }
 
 export default Navbar;
