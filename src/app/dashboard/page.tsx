634959216eac40b30823dc484771c5af4d22fb41
Adrian Davila <akdavila03@gmail.com>
2023-07-09 14:38:10 -0400

    add apollo client

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,21 @@
 import React from "react";
 
-function Page() {
-  return <div>Dashboard</div>;
+import { getClient } from "@/lib/client";
+import { gql } from "@apollo/client";
+
+const query = gql`
+  query GetUser {
+    user(id: 3) {
+      email
+      name
+    }
+  }
+`;
+
+async function Page() {
+  const { data } = await getClient().query({ query });
+  console.log(data);
+  return <div>{data.user.name}</div>;
 }
 
 export default Page;
