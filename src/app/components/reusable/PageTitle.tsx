4f2306e280ea257be62774315d12ed74cebed732
Adrian Davila <akdavila03@gmail.com>
2023-07-10 01:20:11 -0400

    formatting

--- src/app/components/reusable/PageTitle.tsx
+++ src/app/components/reusable/PageTitle.tsx
@@ -4,11 +4,7 @@ import React from "react";
 import { Heading } from "@chakra-ui/react";
 
 function PageTitle({ title }: { title: string }) {
-  return (
-    <Heading fontSize="4xl" as="u">
-      {title}
-    </Heading>
-  );
+  return <Heading fontSize="4xl">{title}</Heading>;
 }
 
 export default PageTitle;
