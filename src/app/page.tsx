72bb04ca5c38bbb33d4427e67db30f9e0612b74c
Adrian Davila <akdavila03@gmail.com>
2023-07-23 22:05:04 -0400

    import graphql queries

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,10 @@ import styles from "./page.module.css";
 
 import Hero from "@/components/landing/Hero";
 
+import { GetUserByUsername } from "@/graphql/queries/User.graphql";
+
 export default function Home() {
+  console.log(GetUserByUsername);
   return (
     <main className={styles.main}>
       <div>
